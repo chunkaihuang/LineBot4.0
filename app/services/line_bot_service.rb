a0775d307a5b24362a4d954e89d0c6c022cd5db1
john <johnwu@tripmoment.com>
2017-05-17 22:14:10 +0800

    custom-response

--- app/services/line_bot_service.rb
+++ app/services/line_bot_service.rb
@@ -35,6 +35,10 @@ class LineBotService
             @return_mag = "比陳進興那些殺人犯還要不如的爛貨！"
           when /蔡竣宇/, /小護士/
             @return_mag = "你們可以自己仔細思考看看，他的這種惡劣行為大概比殺人在緩和一點而已！"
+          when /媽媽/, /小護士/
+            @return_mag = "誰弄到你媽你會放過？"
+          when /時候/, /真心/, /體會/, /感受/
+            @return_mag = "每當這個時候 只要想起 絕不讓真心對自己的人失望 就覺得時時刻刻都是希望 用心去體會 感受"
           else
             ''
           end
@@ -43,11 +47,11 @@ class LineBotService
             text: @return_mag
           }
 
-          puts "回傳訊息: #{@return_mag}"
-          puts "token: #{event['replyToken']}"
+          # puts "回傳訊息: #{@return_mag}"
+          # puts "token: #{event['replyToken']}"
             
           msg = @client.reply_message(event['replyToken'], message)
-          puts "line回傳：#{msg}"
+          # puts "line回傳：#{msg}"
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = @client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
