b9f0788afa58f8f1fe12b9c073e4c80935b02d46
john <johnwu@tripmoment.com>
2017-05-20 09:45:23 +0800

    fix

--- app/services/line_bot_service.rb
+++ app/services/line_bot_service.rb
@@ -103,7 +103,7 @@ class LineBotService
     files = ['evalcookie', 'frommide', 'lin', 'withgirl', 'towu']
     case receive_message
     when /-help/
-      str = "使用方式：\n"+
+      str = "白痴廢物智障靠爸人渣使用方式：\n"+
             "-help 查看所有指令\n"+
             "-ar 隨機文字\n"+"-ap(文字) 搜尋指定文字\n"+
             "-av 讚貼圖\n"+
